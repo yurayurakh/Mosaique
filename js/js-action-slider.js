3ef892a9eaca5f223ccab6e2f95f8b32e3b23b06
Yurii <yurayurakh@mail.ru>
2016-08-14 15:37:10 +0300

    Initial

---
+++ js/js-action-slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+        slideWidth: 144,
+        moveSlides: 1,
+        minSlides: 8,
+        maxSlides: 13,
+        slideMargin: 7
+    });
+});
